99ec35df9217aada51c0855f9313ddb907055f73
JairFlores20 <jair100flo@gmail.com>
2025-03-17 17:37:25 -0600

    Token no necesario

--- src/Estado.java
+++ src/Estado.java
@@ -10,49 +10,24 @@ public class Estado {
 
     private int idEstado1;
     private boolean edoAcept1;
-    private int token1;
     private Set<Transicion> trans1 = new HashSet<>();
 
     public Estado() {
         this.idEstado1 = ContadorIdEstado++;
         this.edoAcept1 = false;
-        this.token1 = -1;
     }
 
     public Set<Transicion> getTrans1() {
         return trans1;
     }
-
     public void setTrans1(Set<Transicion> trans1) {
         this.trans1 = trans1;
     }
-
-    public int getToken() {
-        return token1;
-    }
-
-    public void setToken(int token1) {
-        this.token1 = token1;
-    }
-
-    public boolean getEdoAcept() {
-        return edoAcept1;
-    }
-
-    public void setEdoAcept(boolean edoAcept1) {
-        this.edoAcept1 = edoAcept1;
-    }
-
-    public int getIdEstado() {
-        return idEstado1;
-    }
-
-    public void setIdEstado(int idEstado1) {
-        this.idEstado1 = idEstado1;
-    }
-    public void agregarTransicion(Transicion t) {
-        this.trans1.add(t);
-    }
+    public boolean getEdoAcept() {return edoAcept1;}
+    public void setEdoAcept(boolean edoAcept1) {this.edoAcept1 = edoAcept1;}
+    public int getIdEstado() {return idEstado1;}
+    public void setIdEstado(int idEstado1) {this.idEstado1 = idEstado1;}
+    public void agregarTransicion(Transicion t) {this.trans1.add(t);}
 
 }
 
